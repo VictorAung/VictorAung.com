396a50bb033157e13f8d0d7d363381e5f72e3662
VictorAung <VictorAung2021@u.northwestern.edu>
2019-03-05 02:27:55 -0600

    working index

---
+++ js/clean-blog.js
@@ -0,0 +1,67 @@
+const app = {
+  text : "I am a designer and developer pursuing a B.S. in Computer Science at Northwestern University.",
+  index : 0,
+  chars : 0,
+  speed: 75,
+  container : '.animatedtext .content',
+  init : function() {
+    this.chars = this.text.length;
+    return this.write();
+  },
+  write : function() {
+  let container = document.querySelector(this.container);
+  container.append(this.text[this.index]);
+    if (this.index < this.chars-1) {
+      this.index++;
+      setTimeout(function() {
+        app.write();
+      }, this.speed);
+
+
+
+    }
+  }
+};
+app.init();
+
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Floating label headings for the contact form
+  $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+    $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+  }).on("focus", ".floating-label-form-group", function() {
+    $(this).addClass("floating-label-form-group-with-focus");
+  }).on("blur", ".floating-label-form-group", function() {
+    $(this).removeClass("floating-label-form-group-with-focus");
+  });
+
+  // Show the navbar when the page is scrolled up
+  var MQL = 992;
+
+  //primary navigation slide-in effect
+  if ($(window).width() > MQL) {
+    var headerHeight = $('#mainNav').height();
+    $(window).on('scroll', {
+        previousTop: 0
+      },
+      function() {
+        var currentTop = $(window).scrollTop();
+        //check if user is scrolling up
+        if (currentTop < this.previousTop) {
+          //if scrolling up...
+          if (currentTop > 0 && $('#mainNav').hasClass('is-fixed')) {
+            $('#mainNav').addClass('is-visible');
+          } else {
+            $('#mainNav').removeClass('is-visible is-fixed');
+          }
+        } else if (currentTop > this.previousTop) {
+          //if scrolling down...
+          $('#mainNav').removeClass('is-visible');
+          if (currentTop > headerHeight && !$('#mainNav').hasClass('is-fixed')) $('#mainNav').addClass('is-fixed');
+        }
+        this.previousTop = currentTop;
+      });
+  }
+
+})(jQuery); // End of use strict
